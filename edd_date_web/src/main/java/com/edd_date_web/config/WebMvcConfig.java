f79a2a687996642683129278b86e38ba229b21e4
wofov <hys950526@naver.com>
2023-12-24 09:24:06 +0900

    cors 구성 메소드 추가

--- edd_date_web/src/main/java/com/edd_date_web/config/WebMvcConfig.java
+++ edd_date_web/src/main/java/com/edd_date_web/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.edd_date_web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,4 +18,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(requestLimitInterceptor);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+
 }
